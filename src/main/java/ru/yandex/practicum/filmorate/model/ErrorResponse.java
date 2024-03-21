c37b6a5ec4d46ce807778669340012c0cc4fddd4
stanislavma <msvdev@yandex.com>
2024-03-22 01:31:51 +0500

    style: ErrorResponse.java:9:5: 'CTOR_DEF' should be separated from previous line

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class ErrorResponse {
     // геттеры необходимы, чтобы Spring Boot мог получить значения полей
     String error;
+
     public ErrorResponse(String error) {
         this.error = error;
     }
